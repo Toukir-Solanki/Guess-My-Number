0ada3011f3b72afecd8c9c8dcead1f8d63e008ec
Toukir-Solanki <toukirsolanki@gmail.com>
2024-03-06 10:21:46 +0530

    i have added few code for the project

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-'use strict';
+"use strict";
+
+document.querySelector(".check").addEventListener("click", function () {
+   const guess_value = Number(document.querySelector('.guess').value);
+   console.log(guess_value);
+
+   if (!guess_value){
+    document.querySelector('.message').textContent = "⛔ No Number"
+   }
+
+
+
+
+
+});
+
+
