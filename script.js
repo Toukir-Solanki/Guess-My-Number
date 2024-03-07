cea8de2c04ac1033a91f8a4b54ec26f828c6e3b6
Toukir-Solanki <toukirsolanki@gmail.com>
2024-03-07 17:50:19 +0530

    added status feature and aslo added logic too

--- script.js
+++ script.js
@@ -1,17 +1,43 @@
 "use strict";
 
+// --------- Generated Random numbers ----------//
+const number_display = Math.trunc(Math.random() * 20 + 1);
+document.querySelector(".number").textContent = number_display;
+//-----------------------------------------------------------
+
+//---------------updating the status-------------------------
+let score_board = 20;
+
 document.querySelector(".check").addEventListener("click", function () {
-   const guess_value = Number(document.querySelector('.guess').value);
-   console.log(guess_value);
+  const guess_value = Number(document.querySelector(".guess").value);
 
-   if (!guess_value){
-    document.querySelector('.message').textContent = "⛔ No Number"
+  if (!guess_value) {
+    document.querySelector(".message").textContent = "⛔ No Number";
+  } else if (guess_value === number_display) {
+    document.querySelector(".message").textContent = "✅ Correct Number";
+    document.querySelector('body').style.backgroundColor = '#60b347'
+    document.querySelector('.number').style.width = '30rem'
+  } else if (guess_value > number_display) {
+   if(score_board > 1){
+      document.querySelector(".message").textContent = "📈 Number is too big!";
+      score_board--;
+      document.querySelector(".score").textContent = score_board;
+   }else{
+      document.querySelector(".message").textContent = "💥 Gamer Over Boss :(";
+      document.querySelector(".score").textContent = 0;
    }
-
-
-
-
-
+  } else if (guess_value < number_display) {
+   if(score_board > 1){
+      document.querySelector(".message").textContent = "📉Number is too small!";
+    score_board--;
+    document.querySelector(".score").textContent = score_board;
+   }else{
+      document.querySelector(".message").textContent = "💥 Gamer Over Boss :(";
+      document.querySelector(".score").textContent = 0;
+   }
+    
+  }
 });
-
+//-----------------------------------------------------------
+ 
 
